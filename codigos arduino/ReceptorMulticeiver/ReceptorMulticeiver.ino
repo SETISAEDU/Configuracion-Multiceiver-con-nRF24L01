f55dd6279ab67892d7944135cf79959d23b29263
SETISAEDU <setisa2016@gmail.com>
2016-08-26 14:27:53 -0600

    Codigos para los modulos

---
+++ codigos arduino/ReceptorMulticeiver/ReceptorMulticeiver.ino
@@ -0,0 +1,254 @@
+/*  ----------------------------------------------------------------
+    SETISAEDU
+    Codigo para la configuracion MultiCeiver con NRF24L01.
+    Codigo del PRX (Receptor Maestro y controlador de las alarmas y operaciones)
+  --------------------------------------------------------------------
+*/
+
+#include <SPI.h> // Libreria de SPI para la comunicacion con el modulo nRF24L01
+//Librerias para el control del modulo nRF
+#include <nRF24L01.h> 
+#include <RF24.h>
+
+
+RF24 radio(9, 10);//Declaracion de los pines de control CE y CSN para el modulo, se define "radio"
+
+//Se crean las 6 direcciones posibles para las Pipes P0 - P5; 
+//Las direcciones es un numero de 40bits para este caso se usa uint_64_t de 64bits
+const uint64_t direccion[] = {0x7878787878LL, 0xB3B4B5B6F1LL, 0xB3B4B5B6CDLL, 0xB3B4B5B6A3LL, 0xB3B4B5B60FLL, 0xB3B4B5B605LL };
+                             // En esta parte LL significa LongLong para asegurar el dato de 64bits
+//Variables de estado de las alarmas
+bool Alarma1 = false, Alarma2 = false, Alarma3 = false;
+
+void setup()
+{
+
+  Serial.begin(115200);  //Se inicia el puerto serial
+  Serial.println("Iniciando modulo receptor");
+  Serial.println();
+
+  radio.begin(); //Inicio del modulo nRF24L01+
+//  radio.setRetries(15, 15);// Cinfiguracion del numero maximo de reintentos
+  radio.setPALevel(RF24_PA_MAX);  // Se configura a la maxima potencia
+  radio.setChannel(100); // Apertura del canal especifico
+  
+  //Apertura de las lineas de comunicacion con un maximo de 6 direcciones
+  radio.openReadingPipe(0, direccion[0]);
+  radio.openReadingPipe(1, direccion[1]);
+  radio.openReadingPipe(2, direccion[2]);
+  //para el caso de tenes mas modulos
+  radio.openReadingPipe(3, direccion[3]); 
+  radio.openReadingPipe(4, direccion[4]);
+  radio.openReadingPipe(5, direccion[5]);
+
+  radio.startListening(); //Se inicia en modo de escucha, a la espera de alertas de las alarmas
+}
+
+void loop()
+{
+  byte pipeNum = 0; //Variable para leer que Modulo envio el dato
+  byte dataRX = 0; //Variable para guardar el dato recibido
+
+  while (radio.available(&pipeNum)) { //Verificar si hay datos de los esclavos
+    radio.read(&dataRX, 1 ); //Se lee un byte, ya que hemos configurado solo en envio de una letra
+    Serial.print("Recibido del transmisor: ");
+    Serial.println(pipeNum + 1); // Se imprime el numero del transmisor que envio el dato
+    
+    if (dataRX == 'A') //Letra correspondiente al transmisor 1
+    {
+      Serial.println("********************************************************************");
+      Serial.println("Aviso de alarma 1 activada!");
+      Serial.println("presione A para apagar o realice lo manualmente desde el modulo 1");
+      Serial.println("********************************************************************");
+      Alarma1 = true; // Se activa la impresion de la alarma en el monitor serial
+    } else if (dataRX == 'D') //Letra correspondiente al transmisor 1
+    {
+      Serial.println("********************************************************************");
+      Serial.println("Alarma 1 desactivada manuealmente");
+      Serial.println("********************************************************************");
+      Alarma1 = false; // Se desactiva la impresion de la alarma en el monitor serial
+    } else if (dataRX == 'B') //Letra correspondiente al transmisor 2
+    {
+      Serial.println("********************************************************************");
+      Serial.println("Aviso de alarma 2 activada!");
+      Serial.println("presione B para apagar o realice lo manualmente desde el modulo 2");
+      Serial.println("********************************************************************");
+      Alarma2 = true; // Se activa la impresion de la alarma en el monitor serial
+    } else if (dataRX == 'E') //Letra correspondiente al transmisor 2
+    {
+      Serial.println("********************************************************************");
+      Serial.println("Alarma 2 desactivada manuealmente");
+      Serial.println("********************************************************************");
+      Alarma2 = false; // Se activa la impresion de la alarma en el monitor serial
+    } else if (dataRX == 'C') //Letra correspondiente al transmisor 3
+    {
+      Serial.println("********************************************************************");
+      Serial.println("Aviso de alarma 3 activada!");
+      Serial.println("presione C para apagar o realice lo manualmente desde el modulo 3");
+      Serial.println("********************************************************************");
+      Alarma3 = true; // Se activa la impresion de la alarma en el monitor serial
+    } else if (dataRX == 'F') //Letra correspondiente al transmisor 3
+    {
+      Serial.println("********************************************************************");
+      Serial.println("Alarma 3 desactivada manuealmente");
+      Serial.println("********************************************************************");
+      Alarma3 = false; // Se desactiva la impresion de la alarma en el monitor serial
+    } else //En el caso de recibir un dato erroneo
+    {
+      Serial.println("Se recibio un dato erroneo");
+    }
+  }
+
+  if (Serial.available())
+  { 
+    char data = Serial.read();
+    int opc = Serial.parseInt();
+    bool ok = 0;
+    if (data == 'a' || data == 'A')
+    {
+      radio.stopListening(); // Paramos la escucha para poder escribir
+      radio.openWritingPipe(direccion[0]); //Se abre el puerto de escritura para la direccion del modulo 1
+      if(opc==0)
+      {
+        Serial.println("********************************************************************");
+        Serial.print("Desactivando alarma 1");
+        char fun = 'a';
+        ok = radio.write(&fun, sizeof(fun)); //Envio de la funcion de desactivacion   
+      }else if(opc==1)
+      {
+        Serial.println("********************************************************************");
+        Serial.print("Armando alarma 1");
+        char fun = 'O';
+        ok = radio.write(&fun, sizeof(fun)); //Envio de la funcion de desactivacion 
+      }else if(opc==2)
+      {
+        Serial.println("********************************************************************");
+        Serial.print("Desarmando alarma 1");
+        char fun = 'o';
+        ok = radio.write(&fun, sizeof(fun)); //Envio de la funcion de desactivacion 
+      }else 
+      {
+        Serial.println("********************************************************************");
+        Serial.println("Digite uno de los comandos validos:");
+        Serial.println("A0: Desactivar alarma 1");
+        Serial.println("A1: Armar alarma 1");
+        Serial.println("A2: Desarmar alarma 1");
+      }
+      
+      if (ok)
+      {
+        Serial.println(", eviado...");
+        Alarma1 = false;
+        radio.startListening(); //Se regresa al modo de escucha
+      } else
+      {
+        Serial.println(", fallo en envio!");
+      }
+      Serial.println("********************************************************************");
+    } else  if (data == 'b' || data == 'B')
+    {
+      radio.stopListening(); // Paramos la escucha para poder escribir
+      radio.openWritingPipe(direccion[1]); //Se abre el puerto de escritura para la direccion del modulo 2
+      if(opc==0)
+      {
+        Serial.println("********************************************************************");
+        Serial.print("Desactivando alarma 2"); 
+        char fun = 'b';
+        ok = radio.write(&fun, sizeof(fun)); //Envio de la funcion de desactivacion   
+      }else if(opc==1)
+      {
+        Serial.println("********************************************************************");
+        Serial.print("Armando alarma 2");
+        char fun = 'O';
+        ok = radio.write(&fun, sizeof(fun)); //Envio de la funcion de Armado 
+      }else if(opc==2)
+      {
+        Serial.println("********************************************************************");
+        Serial.print("Desarmando alarma 2");
+        char fun = 'o';
+        ok = radio.write(&fun, sizeof(fun)); //Envio de la funcion de Desarmado 
+      }else 
+      {
+        Serial.println("********************************************************************");
+        Serial.println("Digite uno de los comandos validos:");
+        Serial.println("B0: Desactivar alarma 2");
+        Serial.println("B1: Armar alarma 2");
+        Serial.println("B2: Desarmar alarma 2");
+       
+      }
+      
+      if (ok)
+      {
+        Serial.println(", eviado...");
+        Alarma2 = false;
+        radio.startListening(); //Se regresa al modo de escucha
+      } else
+      {
+        Serial.println(", fallo en envio!");
+      }
+      Serial.println("********************************************************************");
+    } else  if (data == 'c' || data == 'C')
+    {
+      radio.stopListening(); // Paramos la escucha para poder escribir
+      radio.openWritingPipe(direccion[2]); //Se abre el puerto de escritura para la direccion del modulo 3
+      if(opc==0)
+      {
+        Serial.println("********************************************************************");
+        Serial.print("Desactivando alarma 3"); 
+        char fun = 'c';
+        ok = radio.write(&fun, sizeof(fun)); //Envio de la funcion de desactivacion   
+      }else if(opc==1)
+      {
+        Serial.println("********************************************************************");
+        Serial.print("Armando alarma 3");
+        char fun = 'O';
+        ok = radio.write(&fun, sizeof(fun)); //Envio de la funcion de Armado 
+      }else if(opc==2)
+      {
+        Serial.println("********************************************************************");
+        Serial.print("Desarmando alarma 3");
+        char fun = 'o';
+        ok = radio.write(&fun, sizeof(fun)); //Envio de la funcion de Desarmado 
+      }else 
+      {
+        Serial.println("********************************************************************");
+        Serial.println("Digite uno de los comandos validos:");
+        Serial.println("C0: Desactivar alarma 3");
+        Serial.println("C1: Armar alarma 3");
+        Serial.println("C2: Desarmar alarma 3");
+       
+      }
+      
+      if (ok)
+      {
+        Serial.println(", eviado...");
+        Alarma3 = false;
+        
+      } else
+      {
+        Serial.println(", fallo en envio!");
+      }
+      Serial.println("********************************************************************");
+    }
+    radio.startListening(); //Se regresa al modo de escucha
+     
+  }
+/*
+  if (Alarma1 == true)
+  {
+    //acciones que se desean realizar al recibir la alerta de la alarma 1
+  }
+  if (Alarma2 == true)
+  {
+    //acciones que se desean realizar al recibir la alerta de la alarma 2
+  }
+  if (Alarma3 == true)
+  {
+    //acciones que se desean realizar al recibir la alerta de la alarma 3
+  }
+
+*/
+  delay(200);
+}
+
+
---
+++ codigos arduino/TransmisorMulticeiver/TransmisorMulticeiver.ino
@@ -0,0 +1,134 @@
+/*  ----------------------------------------------------------------
+    SETISAEDU
+    Codigo para la configuracion MultiCeiver con NRF24L01.
+    Codigo del PTX1 (Alarma con activacion por final de carrera(pulsador) )
+  --------------------------------------------------------------------
+*/
+#include <SPI.h> //libreria para el uso del modulo SPI
+#include "nRF24L01.h" //Libreria del modulos nRF24L01+
+#include "RF24.h"
+
+RF24 radio(9, 10); //Declaracion de los pines de control CE y CSN para el modulo, se define el objeto "radio"
+
+/* Lista de direcciones de los transmisores 
+direccion1 = 0x7878787878LL (Este nodo) 
+direccion1 = 0xB3B4B5B6F1LL 
+direccion3 = 0xB3B4B5B6CDLL 
+direccion4 = 0xB3B4B5B6A3LL 
+direccion5 = 0xB3B4B5B60FLL 
+direccion6 = 0xB3B4B5B605LL
+*/
+const uint64_t direccion1 = 0x7878787878LL; // En esta parte LL significa LongLong para asegurar el dato de 64bits
+
+int Alarm_out =4; //Pin de salida de la alarma
+int Alarm_modo =5; //Pin indicador de alarma Armada o Desarmada
+
+bool A_state=0; //Variable de estado logico de la salida del la alarma
+bool Alarma=false,modo=true; //Variable de estado de la alarma, variable de modo de actividad
+bool armado=1; /*Variable que se usa para discriminar las peticiones de interrupcion 
+                *que se hagan mientras la alarma esta Desarmada
+                */
+void setup()
+
+{
+
+  pinMode(10, OUTPUT); //Iniciar como salida el pin de control CSN
+  radio.begin(); //Inicio del modulo nRF24L01+
+  //radio.setRetries(15, 15); //Configuracion del numero de intentos
+  radio.setPALevel(RF24_PA_LOW);  // Configuracion en modo de baja potencia
+  radio.setChannel(100); // Apertura del canal de comunicacion
+    //Apertura de la linea de comunicacion, direccion 2
+  radio.openReadingPipe(0, direccion1); 
+  radio.startListening(); // iniciamos en modo de escucha
+
+  //Configuracion de pines de salida de alarma e indicador
+  pinMode(Alarm_out, OUTPUT);
+  pinMode(Alarm_modo, OUTPUT);
+  digitalWrite(Alarm_out,LOW);
+  digitalWrite(Alarm_modo,HIGH); // La alarma inicia activa
+  
+  //Declaracion de interrupciones de el pulsadores y el sensor 
+  pinMode(2, INPUT); //Disparador de la alarmar (Final de carrera, PIR, IR, Luz)
+  pinMode(3, INPUT); //Boton de apagado de alarma
+  attachInterrupt(digitalPinToInterrupt(2), Alarma_On, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), Alarma_Off, RISING);
+
+}
+
+void loop()
+{
+       radio.startListening(); //Se inicia en modo de escucha para poder recibir ordenes del maestro
+      //Espera de peticiones
+      if (radio.available())      // Comprobacion de datos recibidos
+      {
+        char op; //operacion
+        bool done = false;
+      
+        while (!done)   // Espera hasta recibir algo
+        {
+          radio.read( &op, sizeof(op) );
+          if (op > 0)done = true;
+          delay(20);            
+        }
+        //Acciones
+        if (op == 'a') //Apagar alarma, pero siempre esta armada
+        {
+          Alarma = false;
+          digitalWrite(Alarm_out,LOW);
+        } else if (op == 'O') //Armar la alarma, modo On
+        {
+          digitalWrite(Alarm_out,LOW);
+          digitalWrite(Alarm_modo,HIGH);
+          armado=0; //Se pone a cero para obviar las interrupciones
+          interrupts();
+          delay(100);//Espera aque se den las llamadas a interrupcion pendientes
+          armado=1; //Reactivar las acciones de las interrupciones
+          Alarma = false;
+          modo=true; //Poner modo en Alarma Armada
+        } else if (op == 'o') //Desarmar la alarma,modo Off
+        {
+          digitalWrite(Alarm_out,LOW);
+          digitalWrite(Alarm_modo,LOW);
+          noInterrupts();
+          Alarma = false;
+          modo=false; //Poner modo en Alarma Desarmada
+        } 
+      }
+
+                                           //Verificacion si la alarma a sido disparada y
+      if((Alarma==true) &&  (modo==true))  // si esta se encuentra armada (modo = true)                             
+      {
+        A_state=!A_state;  //Cambio de estado de la variable que controla la salida
+        digitalWrite(Alarm_out,A_state); //Escribir el estado en la salida
+        delay(500); //Espera para que el cambio de estados sea apreciable
+      }
+}
+
+//Rutinas de interrupcion
+void Alarma_On() //Alarma disparada
+  {
+    if((armado==1)&&(Alarma==false)) //Verificacion si se deben obviar o no las peticiones de interrupcion
+    {
+    radio.stopListening(); //Paramos el modo de escucha para poder escribir
+    Alarma = true; //Se enciende la funcion de la alarma
+    char fun = 'A';
+    radio.openWritingPipe(direccion1); //Se abre el puerto de escritura
+    radio.write(&fun, 1); //Se envia la notificacion de que la alarma fue activada.
+    //delay(200);
+    }
+  }
+
+void Alarma_Off() //Boton de apagado de alarma disparado
+  {               
+    //Verificacion si se deben obviar o no las peticiones de interrupcion
+    if((armado==1)&&(Alarma==true)) // y a la vez, sí la la alarma esta encendida
+    {
+    radio.stopListening(); //Paramos el modo de escucha para poder escribir
+    Alarma = false; //Se apaga la funcion de la alarma
+    digitalWrite(Alarm_out,LOW); //Se pone la salida apagada
+    char fun = 'D'; //Letra para informar que la alarma se desactivo manualmente
+    radio.openWritingPipe(direccion1); //Se abre el puerto de escritura
+    radio.write(&fun, 1); //Se envia la notificacion de que la alarma fue activada.
+    //delay(200);
+    }
+  }
